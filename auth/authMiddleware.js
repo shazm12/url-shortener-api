434aff1f4199653430bf4d7d083575b51edf1f49
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-02-08 19:45:38 +0530

    chore: logic to either send Unauthorized message or send back to login page;

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,5 +1,14 @@
 export const checkAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) { return next() }
-    res.redirect("/login");
-}
+    if (req.isAuthenticated()) {
+      return next();
+    }
+  
+    // Check if the request is an API call
+    if (req.originalUrl.startsWith("/api")) {
+      return res.status(401).json({ error: "Unauthorized. Please log in." });
+    }
+  
+    // Redirect to login page for non-API requests
+    res.redirect("/auth/login");
+  };
 
