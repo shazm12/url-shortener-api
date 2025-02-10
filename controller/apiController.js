090ec1463c603cc15257a8552fb715e1d2b33df4
shazm12 <shamik.bera2019@vitstudent.ac.in>
2025-02-10 22:30:19 +0530

    feat: created apiController and moved all logic for /api endpoint to this file;

---
+++ controller/apiController.js
@@ -0,0 +1,87 @@
+import { v4 as uuidv4 } from "uuid";
+import { fnv1aHash, base10ToBase62, parseUserAgent } from "../utils/utils.js";
+import Url from "../models/Url.js";
+import userAgent from "user-agent-parser";
+import UrlClick from "../models/UrlClick.js";
+
+export const postShortenUrlData = async (req, res) => {
+  const { longUrl, customAlias, topic } = req.body;
+
+  if (!longUrl) {
+    return res.status(400).send({ error: "Long URL is required!" });
+  }
+
+  const numericId = fnv1aHash(uuidv4());
+  let alias = customAlias || base10ToBase62(numericId.toString());
+  alias = alias.trim().toLowerCase();
+  if (!alias) {
+    return res.status(400).send({ error: "Alias generation failed!" });
+  }
+
+  try {
+    // Check if the user already has an alias for this long URL
+    const existingUrlForUser = await Url.findOne({
+      longUrl,
+      createdBy: req.user?.id || "12345",
+    });
+
+    if (existingUrlForUser) {
+      return res.status(400).send({
+        error: "You already have a shortened URL for this long URL!",
+        existingUrl: existingUrlForUser,
+      });
+    }
+
+    // Check if the alias already exists in the database
+    const existingAlias = await Url.findOne({ alias });
+
+    if (existingAlias) {
+      return res.status(400).send({ error: "Alias already exists!" });
+    }
+
+    const url = new Url({
+      alias: alias,
+      longUrl: longUrl,
+      createdAt: new Date().toUTCString(),
+      createdBy: req.user?.id || "12345",
+      topic: topic || "",
+    });
+
+    await url.save();
+    res.status(200).send({ url });
+  } catch (err) {
+    if (err.code === 11000) {
+      // Duplicate key error
+      return res
+        .status(400)
+        .send({ error: "Alias already exists!", systemErr: err });
+    }
+    res.status(500).send({ error: err.message || "Internal server error" });
+  }
+};
+
+export const getShortenUrlDataAndRedirectToLongUrl = async (req, res) => {
+  try {
+    const { alias } = req.params;
+    const urlObj = await Url.findOne({ alias });
+
+    if (!urlObj) {
+      return res.status(404).json({ error: "URL not found" });
+    }
+
+    const userAgent = parseUserAgent(req.headers["user-agent"]);
+
+    const urlClick = new UrlClick({
+      urlId: urlObj._id,
+      userIp: req.ip,
+      userAgent: req.headers["user-agent"],
+      os: userAgent?.os,
+      device: userAgent?.device,
+    });
+
+    await urlClick.save();
+    res.redirect(urlObj?.longUrl);
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
